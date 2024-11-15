1ca74bbbdb8ee861607dd5b04d5ba6852584845d
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-11-15 22:07:49 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,30 @@ function showMsg(user, msg) {
 }
 showMsg("Привет", "JavaScript!")
 
+
+
+
+function func(param1, param2, param3) {
+    console.log(param1 + param2 + param3)
+}
+func(1 + 2 + 3)
+
+
+function checkVelo(wheels, weight) {
+    if (wheels < 4 && weight < 100) {
+        return "паркофка разрешена"
+    } else {
+        return "паркофка для велосепедоф"
+    }
+}
+wheels, weight = checkVelo(2, 104);
+console.log(wheels, weight);
+
+
+
+
+
+
 function func(n, a) {
     if (n % 2 && a % 2) {
         return n + a;
@@ -11,8 +35,16 @@ function func(n, a) {
 }
 
 
-function func(param1, param2, param3) {
-    console.log(param1 + param2 + param3)
-}
-func(1 + 2 + 3)
 
+function checkVelo(wheels, weight) {
+
+
+    if (wheels < 2 && weight < 100) {
+
+        return "Парковка разрешена"
+    } else {
+        return "Парковка только для велосипедов"
+    }
+}
+let wheels, weight = checkVelo(4, 104);
+console.log(wheels, weight);
