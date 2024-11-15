b8a09cbd55621fa233fdfdfbbc0d6771f143dee5
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-11-15 21:44:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function showMsg(user, msg) {
+    console.log(`${user},${msg}`);
+}
+showMsg("Привет", "JavaScript!")
+
+function func(n, a) {
+    if (n % 2 && a % 2) {
+        return n + a;
+    } else (n % 2 && a % 2)
+    return n * a
+}
+
+
+function func(param1, param2, param3) {
+    console.log(param1 + param2 + param3)
+}
+func(1 + 2 + 3)
+
